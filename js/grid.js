9ac17900f7183cbebe9fc528124311587add8df2
Mary <mary.becica@appdirect.com>
2012-01-05 21:39:24 -0800

    looks better

--- js/grid.js
+++ js/grid.js
@@ -1,19 +1,3 @@
-//
-// Copyright 2011, Boundary
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-
 (function(undefined) {
 
   window.grid = Backbone.View.extend({
@@ -29,7 +13,7 @@
           id: col,
           name: function() { if (self.alias) { return self.alias[col]; } else { return col; } }(), // how to do aliasing?
           field: col,
-          width: function() { if (col == "name") { return 320; } else if (col == "group") { return 180; } else { return 80; }}()
+          width: 170 
         }
       });
       
